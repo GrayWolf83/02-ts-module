5f36174039db61160bfe0b5cdec9aead7cd72940
Сергей Бернякович <sergejbernakovic@MacBook-Air-Sergej.local>
2023-02-21 16:05:22 +0500

    task-4

---
+++ src/task-4.ts
@@ -0,0 +1,21 @@
+import { RequestInfo, RequestInit } from 'node-fetch'
+const fetch = (url: RequestInfo, init?: RequestInit) =>
+	import('node-fetch').then((module) => module.default(url, init))
+
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments'
+
+interface Comment {
+	id: number
+	email: string
+}
+
+const getData = async (url: string): Promise<Comment[]> => {
+	const response = await fetch(url)
+	return (await response.json()) as Comment[]
+}
+
+getData(COMMENTS_URL).then((data: Comment[]) => {
+	data.forEach((item) => {
+		console.log(`ID: ${item.id}, Email: ${item.email}`)
+	})
+})
